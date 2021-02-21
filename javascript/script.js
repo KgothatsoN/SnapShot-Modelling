33394e42fe668b6fedbb18cbf6146e49a30b3b32
KgothatsoN <kg.otha@hotmail.com>
2021-02-21 04:04:08 +0000

    Commenting

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,9 @@ let email = document.getElementById('email');
 let form = document.getElementById('form');
 let errorElement = document.getElementById('error')
 
+// Form validation
 form.addEventListener('submit', (e) => {
-
+    // Check if email field is empty
     if (email.value === "") {
         e.preventDefault();
         alert('Email Address is required...');
